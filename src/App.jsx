5a83925b7d2b302f4bfa6a0bdb490b8bbc92531f
hinestrozadayana870-lang <hinestrozadayana870@gmail.com>
2026-09-24 11:34:11 -0500

    Convertir tienda en hoja de vida

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from "react";
 import Header from "./components/Header";
-import Inicio from "./components/Inicio";
-import Categorias from "./components/Categorias";
-import Productos from "./components/Productos";
-import Carrito from "./components/Carrito";
+import Perfil from "./components/Perfil";
+import Formacion from "./components/Formacion";
+import Habilidades from "./components/Habilidades";
+import Proyectos from "./components/Proyectos";
 import Contacto from "./components/Contacto";
 import Footer from "./components/Footer";
 
 function App() {
-  const [carrito, setCarrito] = useState([]);
-
-  const agregarAlCarrito = (producto) => {
-    setCarrito((productosActuales) => [...productosActuales, producto]);
-  };
-
-  const vaciarCarrito = () => {
-    setCarrito([]);
-  };
-
-  const total = carrito.reduce((acumulado, producto) => acumulado + producto.precio, 0);
-
   return (
     <>
-      <Header cantidad={carrito.length} />
+      <Header />
       <main>
-        <Inicio />
-        <Categorias />
-        <Productos agregarAlCarrito={agregarAlCarrito} />
-        <Carrito productos={carrito} total={total} vaciarCarrito={vaciarCarrito} />
+        <Perfil />
+        <Formacion />
+        <Habilidades />
+        <Proyectos />
         <Contacto />
       </main>
       <Footer />
