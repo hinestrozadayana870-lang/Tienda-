febe25f4d638e384b08a230a4e608c66a7db5fe2
hinestrozadayana870-lang <hinestrozadayana870@gmail.com>
2026-09-24 11:59:47 -0500

    Convertir hoja de vida en tienda

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
+import { useState } from "react";
 import Header from "./components/Header";
-import Perfil from "./components/Perfil";
-import Formacion from "./components/Formacion";
-import Habilidades from "./components/Habilidades";
-import Proyectos from "./components/Proyectos";
+import Inicio from "./components/Inicio";
+import Categorias from "./components/Categorias";
+import Productos from "./components/Productos";
+import Carrito from "./components/Carrito";
 import Contacto from "./components/Contacto";
 import Footer from "./components/Footer";
 
 function App() {
+  const [carrito, setCarrito] = useState([]);
+
+  const agregarAlCarrito = (producto) => {
+    setCarrito((actual) => [...actual, producto]);
+  };
+
+  const eliminarDelCarrito = (indice) => {
+    setCarrito((actual) => actual.filter((_, posicion) => posicion !== indice));
+  };
+
+  const vaciarCarrito = () => setCarrito([]);
+
+  const total = carrito.reduce((suma, producto) => suma + producto.precio, 0);
+
   return (
     <>
-      <Header />
+      <Header cantidad={carrito.length} />
       <main>
-        <Perfil />
-        <Formacion />
-        <Habilidades />
-        <Proyectos />
+        <Inicio />
+        <Categorias />
+        <Productos agregarAlCarrito={agregarAlCarrito} />
+        <Carrito
+          productos={carrito}
+          total={total}
+          eliminarDelCarrito={eliminarDelCarrito}
+          vaciarCarrito={vaciarCarrito}
+        />
         <Contacto />
       </main>
       <Footer />
