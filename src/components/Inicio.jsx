20e63f00d8be862817749869cfdb5709e2b3700e
hinestrozadayana870-lang <hinestrozadayana870@gmail.com>
2026-09-24 11:34:57 -0500

    Limpiar componentes de tienda

--- src/components/Inicio.jsx
+++
@@ -1,9 +0,0 @@
-const Inicio = () => {
-  return (
-    <section className="sr-only" aria-hidden="true">
-      <h1>Hoja de vida de Leydi Dayana Hinestroza</h1>
-    </section>
-  );
-};
-
-export default Inicio;
